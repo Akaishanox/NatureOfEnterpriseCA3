1bd30c7d158841943795d3d24804b50e105a22ae
Akaishanox <A00032309@myTUDublin.ie>
2026-04-24 02:37:05 +0100

    Create page.tsx

---
+++ web/app/helpdesk/page.tsx
@@ -0,0 +1,8 @@
+export default function HelpdeskPage() {
+  return (
+    <main className="page">
+      <h1>Helpdesk</h1>
+      <p>Submit and track your support tickets here.</p>
+    </main>
+  );
+}
