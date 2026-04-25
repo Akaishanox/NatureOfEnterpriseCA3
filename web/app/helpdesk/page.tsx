e6e3c4a47008c5bac85ef915901b3c52d3d7e195
Akaishanox <A00032309@myTUDublin.ie>
2026-04-25 18:39:12 +0100

    Deleted placeholder helpdesk page

--- web/app/helpdesk/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HelpdeskPage() {
-  return (
-    <main className="page">
-      <h1>Helpdesk</h1>
-      <p>Submit and track your support tickets here.</p>
-    </main>
-  );
-}
