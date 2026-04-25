edbcc4479919e7944f53f42c629624094df21a7e
Akaishanox <A00032309@myTUDublin.ie>
2026-04-25 20:02:14 +0100

    Delete web/app/page.tsx

--- web/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="page">
-      <h1>Campus Companion</h1>
-      <p>Welcome to the campus app. Use the navigation to explore features.</p>
-    </main>
-  );
-}
